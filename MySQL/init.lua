bb5675a2656574d34b057c9ae38f694db95f0260
U-CANDYMI\Candy <869646063@qq.com>
2020-12-18 16:10:03 +0800

    初始化代码

--- MySQL/init.lua
+++ MySQL/init.lua
@@ -9,48 +9,48 @@ local Delete = require "Model.Mysql.Delete"
 local type = type
 local assert = assert
 
-	-- 对象模型
-	local Model = class("Model")
+-- 对象模型
+local Model = class("Model")
 
-	function Model:ctor(opt)
-		self.tname = opt.tname
-		self.fields = opt.fields
-	end
+function Model:ctor(opt)
+	self.tname = opt.tname
+	self.fields = opt.fields
+end
 
-	-- 传递数据库连接对象
-	function Model:setDB(DB)
-		self.db = assert(DB, "Invalid DB Class.")
-	end
-	
-	-- 开始建表
-	function Model:createTable()
-		-- TODO
-		local query = fmt("CREATE TABLE IF NOT EXISTS `%s`(\n%s\n)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", self.tname)
-	end
+-- 传递数据库连接对象
+function Model:setDB(DB)
+	self.db = assert(DB, "Invalid DB Class.")
+end
 
-	-- 查询语句
-	function Model:find(tab)
-		-- TODO
-		return Select:new()
-	end
+-- 开始建表
+function Model:createTable()
+	-- TODO
+	local query = fmt("CREATE TABLE IF NOT EXISTS `%s`(\n%s\n)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", self.tname)
+end
 
-	-- 插入语句
-	function Model:insert(tab)
-		-- TODO
-		return Insert:new()
-	end
+-- 查询语句
+function Model:find(tab)
+	-- TODO
+	return Select:new()
+end
 
-	-- 更新语句
-	function Model:update(tab)
-		-- TODO
-		return Update:new()
-	end
+-- 插入语句
+function Model:insert(tab)
+	-- TODO
+	return Insert:new()
+end
 
-	-- 删除语句
-	function Model:delete(tab)
-		-- TODO
-		return Delete:new()
-	end
+-- 更新语句
+function Model:update(tab)
+	-- TODO
+	return Update:new()
+end
+
+-- 删除语句
+function Model:delete(tab)
+	-- TODO
+	return Delete:new()
+end
 
 -- 模型初始化方法
 return function (tname, fields)
